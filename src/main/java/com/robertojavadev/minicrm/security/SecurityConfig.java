3238e370d96d604d76d2033b9406faa029b76eb6
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-03 21:25:00 +0200

    refactore: Refactored fragments.html

--- src/main/java/com/robertojavadev/minicrm/security/SecurityConfig.java
+++ src/main/java/com/robertojavadev/minicrm/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 )
                 .formLogin(login -> login
                         .loginPage("/").permitAll()
-                        .defaultSuccessUrl("/disasters/add")
+                        .defaultSuccessUrl("/crm/index")
                         .loginProcessingUrl("/loginproc")
                 )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
