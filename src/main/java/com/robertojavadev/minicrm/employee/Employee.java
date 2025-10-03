3a92c69d1426848270434563871cef2a0d37470d
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-03 14:45:02 +0200

    refactor: Renamed field names in entity

--- src/main/java/com/robertojavadev/minicrm/employee/Employee.java
+++ src/main/java/com/robertojavadev/minicrm/employee/Employee.java
@@ -1,7 +1,11 @@
 package com.robertojavadev.minicrm.employee;
 
+import com.robertojavadev.minicrm.company.Company;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,4 +27,16 @@ class Employee {
     @UuidGenerator(style = UuidGenerator.Style.AUTO)
     private UUID id;
 
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    private String email;
+    private String phone;
 }
