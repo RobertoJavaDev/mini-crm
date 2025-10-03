f81ca0a92e55beb16133ead19ff8520b7f60d899
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-03 17:05:31 +0200

    feat: Implemented security methods to user service

--- src/main/java/com/robertojavadev/minicrm/user/UserService.java
+++ src/main/java/com/robertojavadev/minicrm/user/UserService.java
@@ -2,6 +2,7 @@ package com.robertojavadev.minicrm.user;
 
 import com.robertojavadev.minicrm.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -10,8 +11,9 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-class UserService {
+public class UserService {
 
+    public static final String USER_WITH_EMAIL_DOES_NOT_EXIST = "User with email: %d does not exist";
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
@@ -19,6 +21,16 @@ class UserService {
         return userRepository.findAll().stream()
                 .map(userMapper::mapUserToUserDto)
                 .collect(Collectors.toSet());
+    }
+
+    public UserDto findByEmail(String email) {
+        User user = findUserByEmail(email);
+        return userMapper.mapUserToUserDto(user);
+    }
+
+    private User findUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(USER_WITH_EMAIL_DOES_NOT_EXIST + email));
     }
 
     public void delete(UUID id) {
