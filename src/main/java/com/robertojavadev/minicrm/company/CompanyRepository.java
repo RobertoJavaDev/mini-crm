1c51ad7f02ea02cf7759b7daeffa25d5e21329d2
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-03 13:14:13 +0200

    feat: Added repository interface

--- src/main/java/com/robertojavadev/minicrm/company/CompanyRepository.java
+++ src/main/java/com/robertojavadev/minicrm/company/CompanyRepository.java
@@ -1,4 +1,8 @@
 package com.robertojavadev.minicrm.company;
 
-public interface CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
 }
