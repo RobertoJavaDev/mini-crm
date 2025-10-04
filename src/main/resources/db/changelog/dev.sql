7de6ac30c11433a9adcc99ce0a68d007d52f8a12
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-04 16:51:02 +0200

    feat: Added data with emploeeys to database

--- src/main/resources/db/changelog/dev.sql
+++ src/main/resources/db/changelog/dev.sql
@@ -45,57 +45,156 @@ VALUES (gen_random_uuid(),
 
 -- changeset RobertoJavaDev:5
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'TechNova Sp. z o.o.',
-     'kontakt@technova.pl',
-     '/uploads/logos/technova.png',
-     'https://www.technova.pl');
+VALUES (gen_random_uuid(),
+        'TechNova Sp. z o.o.',
+        'kontakt@technova.pl',
+        '/uploads/logos/technova.png',
+        'https://www.technova.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'GreenFuture S.A.',
-     'info@greenfuture.pl',
-     '/uploads/logos/greenfuture.png',
-     'https://www.greenfuture.pl');
+VALUES (gen_random_uuid(),
+        'GreenFuture S.A.',
+        'info@greenfuture.pl',
+        '/uploads/logos/greenfuture.png',
+        'https://www.greenfuture.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'BlueOcean Tech',
-     'biuro@blueocean.pl',
-     '/uploads/logos/blueocean.png',
-     'https://www.blueocean.pl');
+VALUES (gen_random_uuid(),
+        'BlueOcean Tech',
+        'biuro@blueocean.pl',
+        '/uploads/logos/blueocean.png',
+        'https://www.blueocean.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'NextGen Innovations',
-     'kontakt@nextgen.pl',
-     '/uploads/logos/nextgen.png',
-     'https://www.nextgen.pl');
+VALUES (gen_random_uuid(),
+        'NextGen Innovations',
+        'kontakt@nextgen.pl',
+        '/uploads/logos/nextgen.png',
+        'https://www.nextgen.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'PolSoft Systems',
-     'office@polsoft.pl',
-     '/uploads/logos/polsoft.png',
-     'https://www.polsoft.pl');
+VALUES (gen_random_uuid(),
+        'PolSoft Systems',
+        'office@polsoft.pl',
+        '/uploads/logos/polsoft.png',
+        'https://www.polsoft.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'DataWorks Polska',
-     'support@dataworks.pl',
-     '/uploads/logos/dataworks.png',
-     'https://www.dataworks.pl');
+VALUES (gen_random_uuid(),
+        'DataWorks Polska',
+        'support@dataworks.pl',
+        '/uploads/logos/dataworks.png',
+        'https://www.dataworks.pl');
 
 INSERT INTO companies (id, company_name, email, logo_filename, website)
-VALUES
-    (gen_random_uuid(),
-     'Skyline Solutions',
-     'info@skyline.pl',
-     '/uploads/logos/skyline.png',
-     'https://www.skyline.pl');
+VALUES (gen_random_uuid(),
+        'Skyline Solutions',
+        'info@skyline.pl',
+        '/uploads/logos/skyline.png',
+        'https://www.skyline.pl');
+
+
+-- changeset RobertoJavaDev:6
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Jan',
+        'Kowalski',
+        '2b98ef6c-2b29-47d6-ae6f-6926dfbd286f',
+        'jan.kowalski@technova.pl',
+        '+48 600 123 456');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Anna',
+        'Nowak',
+        '2b98ef6c-2b29-47d6-ae6f-6926dfbd286f',
+        'anna.nowak@technova.pl',
+        '+48 601 234 567');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Tomasz',
+        'Wiśniewski',
+        '98085ffd-3150-44d6-aa1c-bf77eae3488c',
+        'tomasz.wisniewski@greenfuture.pl',
+        '+48 602 345 678');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Katarzyna',
+        'Mazur',
+        '98085ffd-3150-44d6-aa1c-bf77eae3488c',
+        'katarzyna.mazur@greenfuture.pl',
+        '+48 603 456 789');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Paweł',
+        'Zieliński',
+        '98085ffd-3150-44d6-aa1c-bf77eae3488c',
+        'pawel.zielinski@blueocean.pl',
+        '+48 604 567 890');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Monika',
+        'Wójcik',
+        'e52746df-7607-425c-b2a1-c2ef9e4fd72a',
+        'monika.wojcik@blueocean.pl',
+        '+48 605 678 901');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Marek',
+        'Krawczyk',
+        '2e0b7405-3ca7-44fd-b439-161d2084dafc',
+        'marek.krawczyk@nextgen.pl',
+        '+48 606 789 012');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Alicja',
+        'Piotrowska',
+        '2e0b7405-3ca7-44fd-b439-161d2084dafc',
+        'alicja.piotrowska@nextgen.pl',
+        '+48 607 890 123');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Robert',
+        'Grabowski',
+        'bebf1c06-c1e9-41af-b485-3c08bad65153',
+        'robert.grabowski@polsoft.pl',
+        '+48 608 901 234');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Ewa',
+        'Kubiak',
+        '337376b1-8dce-457b-a0c8-a2db94cc0ea6',
+        'ewa.kubiak@dataworks.pl',
+        '+48 609 012 345');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Michał',
+        'Dąbrowski',
+        '337376b1-8dce-457b-a0c8-a2db94cc0ea6',
+        'michal.dabrowski@dataworks.pl',
+        '+48 610 123 456');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Joanna',
+        'Czarnecka',
+        '1b178163-1215-4ce3-9a14-5cf5b00018fd',
+        'joanna.czarnecka@skyline.pl',
+        '+48 611 234 567');
+
+INSERT INTO employees (id, first_name, last_name, company_id, email, phone)
+VALUES (gen_random_uuid(),
+        'Piotr',
+        'Lis',
+        '1b178163-1215-4ce3-9a14-5cf5b00018fd',
+        'piotr.lis@skyline.pl',
+        '+48 612 345 678');
