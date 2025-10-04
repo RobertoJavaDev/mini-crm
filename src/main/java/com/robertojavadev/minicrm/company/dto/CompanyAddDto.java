bdd3c5905e332652f8ce57ca620ae7197a4ba531
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-04 11:28:23 +0200

    feat: Added validation for company add dto record

--- src/main/java/com/robertojavadev/minicrm/company/dto/CompanyAddDto.java
+++ src/main/java/com/robertojavadev/minicrm/company/dto/CompanyAddDto.java
@@ -1,7 +1,26 @@
 package com.robertojavadev.minicrm.company.dto;
 
-public record CompanyAddDto(String companyName,
-                            String email,
-                            String logoFilename,
-                            String website) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record CompanyAddDto(
+        @NotBlank()
+        @Size(min = 2, max = 255)
+        String companyName,
+
+        @Email()
+        @Size(max = 255)
+        String email,
+
+        @Size(max = 255)
+        String logoFilename,
+
+        @Pattern(
+                regexp = "^(https?://)?([\\w.-]+)\\.([a-z]{2,})([/\\w .-]*)*/?$",
+                message = "Podaj poprawny adres strony internetowej"
+        )
+        @Size(max = 255)
+        String website) {
 }
