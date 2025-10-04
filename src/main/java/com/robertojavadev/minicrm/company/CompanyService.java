2c244aec7d5cf996798b3171d21ba09f01e57357
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-04 10:39:15 +0200

    feat: Added company service interface

--- src/main/java/com/robertojavadev/minicrm/company/CompanyService.java
+++ src/main/java/com/robertojavadev/minicrm/company/CompanyService.java
@@ -1,49 +1,22 @@
 package com.robertojavadev.minicrm.company;
 
 import com.robertojavadev.minicrm.company.dto.CompanyDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import jakarta.validation.Valid;
+import lombok.NonNull;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-@Service
-@RequiredArgsConstructor
-public class CompanyService {
+@Validated
+interface CompanyService {
+    CompanyDto createCompany(@NonNull @Valid CompanyDto companyDto);
 
-    public static final String COMPANY_WITH_ID_DOES_NOT_EXIST = "Company with id: %s does not exist";
-    private final CompanyRepository companyRepository;
-    private final CompanyMapper companyMapper;
+    List<CompanyDto> findAllCompanies();
 
-    public CompanyDto createCompany(CompanyDto companyDto) {
-        Company company = companyMapper.mapCompanyDtoToCompany(companyDto);
-        return companyMapper.mapCompanyToCompanyDto(companyRepository.save(company));
-    }
+    CompanyDto findCompanyById(@NonNull UUID companyId);
 
-    public List<CompanyDto> getAllCompanies() {
-        List<Company> companies = companyRepository.findAll();
-        return companies.stream().map(companyMapper::mapCompanyToCompanyDto).collect(Collectors.toList());
-    }
+    CompanyDto updateCompany(@NonNull UUID companyId, @NonNull @Valid CompanyDto companyDto);
 
-    public CompanyDto getCompanyById(UUID id) {
-        return companyMapper.mapCompanyToCompanyDto(companyRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException(COMPANY_WITH_ID_DOES_NOT_EXIST + id)));
-    }
-
-    public CompanyDto updateCompany(UUID id, CompanyDto companyDto) {
-        Company company = companyRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException(String.format(COMPANY_WITH_ID_DOES_NOT_EXIST, id)));
-
-        company.setCompanyName(companyDto.companyName());
-        company.setEmail(companyDto.email());
-        company.setLogoFilename(companyDto.logoFilename());
-        company.setWebsite(companyDto.website());
-
-        return companyMapper.mapCompanyToCompanyDto(companyRepository.save(company));
-    }
-
-    public void deleteCompanyById(UUID id) {
-        companyRepository.deleteById(id);
-    }
+    void deleteCompanyById(@NonNull UUID companyId);
 }
