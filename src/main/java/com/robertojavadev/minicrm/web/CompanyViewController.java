a4f3e54433b175d724919228b92e00248d1263b7
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-04 15:20:31 +0200

    feat: Changed implementation for company view controller

--- src/main/java/com/robertojavadev/minicrm/web/CompanyViewController.java
+++ src/main/java/com/robertojavadev/minicrm/web/CompanyViewController.java
@@ -1,10 +1,15 @@
 package com.robertojavadev.minicrm.web;
 
 import com.robertojavadev.minicrm.company.CompanyFacade;
+import com.robertojavadev.minicrm.company.dto.CompanyDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +18,9 @@ public class CompanyViewController {
     private final CompanyFacade companyFacade;
 
     @GetMapping("/list")
-    public String listCompanies() {
+    public String listCompanies(Model model) {
+        List<CompanyDto> companies = companyFacade.findAllCompanies();
+        model.addAttribute("companies", companies);
         return "companies/list";
     }
 }
