385b8b5acdbbb006cff401c93739fb122cbb631a
RobertoJavaDev <robertojavadev@gmail.com>
2025-10-05 16:33:36 +0200

    fix: Fixed bug with add new employee

--- src/main/java/com/robertojavadev/minicrm/_config/WebConfig.java
+++ src/main/java/com/robertojavadev/minicrm/_config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.robertojavadev.minicrm._config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final StringToCompanyConverter stringToCompanyConverter;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -13,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("classpath:/static/uploads/")
                 .addResourceLocations("file:/app/uploads/");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(stringToCompanyConverter);
     }
 }
